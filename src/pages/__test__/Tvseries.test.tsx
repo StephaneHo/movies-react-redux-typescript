abccea1e98d11106f923f4a44e168c3b1bb202da
StephaneHo <stephane.hordoir@gmail.com>
2023-03-14 13:54:53 +0100

    add more tests

--- src/pages/__test__/Tvseries.test.tsx
+++ src/pages/__test__/Tvseries.test.tsx
@@ -2,11 +2,11 @@ import { render, waitFor, screen } from "@testing-library/react";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 import { Provider } from "react-redux";
-import { Movies } from "../Movies";
 import { store } from "../../app/store";
 
 import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Tvseries } from "../Tvseries";
 
 interface LoginBody {
   username: string;
@@ -17,21 +17,21 @@ interface LoginResponse {
 }
 const server = setupServer(
   rest.get<LoginBody, LoginResponse>(
-    "https://api.themoviedb.org/3/discover/movie",
+    "https://api.themoviedb.org/3/discover/tv",
     (req, res, ctx) => {
       return res(
         ctx.json({
-          results: [{ title: "Star Wars", id: "1", backdrop_path: "123" }],
+          results: [{ name: "The Mandalorian", id: "1", backdrop_path: "123" }],
         })
       );
     }
   ),
   rest.get<LoginBody, LoginResponse>(
-    "https://api.themoviedb.org/3/search/movie",
+    "https://api.themoviedb.org/3/search/tv",
     (req, res, ctx) => {
       return res(
         ctx.json({
-          results: [{ title: "Star Wars", id: "1", backdrop_path: "123" }],
+          results: [{ name: "The Mandalorian", id: "1", backdrop_path: "123" }],
         })
       );
     }
@@ -44,9 +44,9 @@ afterAll(() => server.close());
 test("renders Movies", async () => {
   render(
     <Provider store={store}>
-      <Movies />
+      <Tvseries />
     </Provider>,
     { wrapper: BrowserRouter }
   );
-  await waitFor(() => screen.getByText("Star Wars"));
+  await waitFor(() => screen.getByText("The Mandalorian"));
 });
